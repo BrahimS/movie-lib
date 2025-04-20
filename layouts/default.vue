329ca2ac25620c34a510ff61ba7a4037dcfd3355
brahims <bsourny@gmail.com>
2025-04-20 20:25:02 +0200

    feat(layout): create main layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineOptions({
+  name: 'DefaultLayout',
+})
+</script>
+<template>
+  <v-app>
+    <v-main class="tw-bg-gray-900">
+      <slot />
+    </v-main>
+  </v-app>
+</template>
