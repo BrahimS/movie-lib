422c5b09c697c8d2c6908fa7a2a601c99386a940
brahims <bsourny@gmail.com>
2025-04-20 20:44:30 +0200

    feat(ui): implement back to page button

---
+++ components/ui/BackToMoviesButton.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'BackToMoviesButton',
+})
+defineProps<{
+  to?: string
+  text?: string
+}>()
+</script>
+
+<template>
+  <v-row class="tw-my-8">
+    <v-col cols="12">
+      <v-btn
+        color="primary"
+        variant="text"
+        size="large"
+        :to="to || '/'"
+        prepend-icon="mdi-arrow-left"
+      >
+        {{ text || 'Back to Movies' }}
+      </v-btn>
+    </v-col>
+  </v-row>
+</template>
